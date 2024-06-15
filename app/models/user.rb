11b1a8977167d7d3a65f4e601ba1fb1023be50bc
Eddie Wen <wenyuan929@outlook.com>
2024-06-16 00:09:54 +0800

    added user and post modals and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   validates :username, presence: true, uniqueness: true, length: { in: 2..12 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
