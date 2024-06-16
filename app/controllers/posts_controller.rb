126d7d1fd0558f75ccca23192f260097463f91e5
Eddie Wen <wenyuan929@outlook.com>
2024-06-16 22:31:47 +0800

    project finished.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,23 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     if @post.save
-      redirect_to @post
+      redirect_to posts_path, notice: 'Post was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
